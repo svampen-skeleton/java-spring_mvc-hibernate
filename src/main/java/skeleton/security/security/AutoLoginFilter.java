937b7822884583c4db2ed584274f7b739587d1db
Rasmus Jönsson <52160907+svampen-skeleton@users.noreply.github.com>
2019-09-29 12:57:21 +0200

    Adh name change

---
+++ src/main/java/skeleton/security/security/AutoLoginFilter.java
@@ -0,0 +1,19 @@
+package skeleton.security;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
+
+public class AutoLoginFilter extends AbstractPreAuthenticatedProcessingFilter {
+
+	@Override
+	protected Object getPreAuthenticatedCredentials(HttpServletRequest request) {
+		return "IGNORE";
+	}
+
+	@Override
+	protected Object getPreAuthenticatedPrincipal(HttpServletRequest request) {
+		return "admin";
+	}
+
+}
